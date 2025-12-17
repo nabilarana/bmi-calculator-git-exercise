09edb3fb4f97d27a9a26dd322ea6d5d2470026b5
Nabila Rana Pradipta <nabilaranapradipta20@gmail.com>
2025-12-17 21:14:41 +0700

    Add BMI category calculation feature

--- script.js
+++ script.js
@@ -6,3 +6,30 @@ function bmi(){
 	document.getElementById("result").innerHTML="Your BMI is "+bmio;
 }
 
+function calculateBMI() {
+	const height = parseFloat(document.getElementById('h').value);
+	const weight = parseFloat(document.getElementById('w').value);
+
+	if (!height || !weight || height <= 0 || weight <= 0) {
+		document.getElementById("result").innerHTML = "Please enter valid values!";
+		return;
+	}
+
+	const bmi = weight / ((height / 100) * (height / 100));
+	const bmiValue = bmi.toFixed(1);
+
+	let category = "";
+
+	if (bmi < 18.5) {
+		category = "Underweight";
+	} else if (bmi < 25) {
+		category = "Normal Weight";
+	} else if (bmi < 30) {
+		category = "Overweight";
+	} else {
+		category = "Obese";
+	}
+
+	document.getElementById("result").innerHTML =
+		`Your BMI is ${bmiValue}<br>Category: ${category}`;
+}
